9954e89a892ccc9cf5db496df9e5ab3d7871de89
Rebecca <rebeccalagerr@outlook.com>
2023-10-06 00:21:38 +0200

    Ja, men nu funkar ju hamburgemenyn...

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 
-document.querySelector('.hamburger').addEventListener('click', (e) => {
-    console.log(e)
+// document.querySelector('.hamburger').addEventListener('click', (e) => {
+//     console.log(e)
 
-    document.querySelector('.hamburger-items').classList.toggle('open')
-})
+//     document.querySelector('.hamburger-items').classList.toggle('open')
+// })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menu = document.querySelector(".hamburger");
+
+    menu.addEventListener("click", function () {
+        const hamburgerItems = document.querySelector(".hamburger-items");
+        hamburgerItems.classList.toggle("open");
+        menu.classList.toggle("open");
+    });
+});
